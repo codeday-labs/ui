0583d9295ee228cf3b0731e228651f47bef1afd9
Alex Thomsen <alex.scott.thomsen@gmail.com>
2022-02-03 15:10:58 -0700

    Initial commit

--- src/lib/stores/refreshable.ts
+++ src/lib/stores/refreshable.ts
@@ -6,32 +6,115 @@ type SetIntervalType = typeof setInterval;
 type Timer = ReturnType<SetIntervalType>;
 type Timeout = Parameters<SetIntervalType>[1];
 
-export interface Refreshable<T> extends Readable<PromiseLike<T>> {
+export interface Refreshable<T> extends Readable<RefreshableStore<T>> {
   refresh: () => void;
+  pause: () => void;
+  restart: () => void;
+}
+interface RefreshableStore<T> {
+  data: Promise<T> | T;
+  timeRemaining: number;
+  percentCompleted: number;
+  startTime: number;
+  endTime: number;
+  currentState: 'error' | 'success' | 'other';
 }
 
 export const refreshable = <T>(
-  callback: () => PromiseLike<T>,
-  initialData: PromiseLike<T> | T = callback(),
+  callback: () => Promise<T>,
+  initialData: Promise<T> | T = callback(),
   timeout: Timeout = 10000,
 ): Refreshable<T> => {
   let interval: Timer;
 
+  const heartBeatInterval = 75;
+  let startTime = Date.now();
+
   const refresh = () => {
-    callback().then((data: T) => store.set(Promise.resolve(data)));
+    callback()
+      .then((data: T) =>
+        store.set({
+          data: Promise.resolve(data),
+          get timeRemaining() {
+            return this.endTime - Date.now();
+          },
+          get percentCompleted() {
+            return Math.floor(((timeout - this.timeRemaining) / timeout) * 100);
+          },
+          startTime: Date.now(),
+          endTime: Date.now() + timeout,
+          currentState: 'success',
+        }),
+      )
+      .catch(() => {
+        store.set({
+          data: Promise.resolve(null),
+          get timeRemaining() {
+            return this.endTime - Date.now();
+          },
+          get percentCompleted() {
+            return Math.floor(((timeout - this.timeRemaining) / timeout) * 100);
+          },
+          startTime: Date.now(),
+          endTime: Date.now() + timeout,
+          currentState: 'error',
+        });
+      });
+  };
+
+  const tick = async () => {
+    const timeSinceStart = Date.now() - startTime;
+
+    if (timeSinceStart >= timeout) {
+      // Hit callback for
+      await refresh();
+      startTime = Date.now();
+    }
+    // This is weird but it triggers the store to update so our derived values
+    // will show up in UI's
+    store.update((store) => {
+      return store;
+    });
+    interval = setTimeout(tick, heartBeatInterval);
+  };
+
+  // This restarts without taking into consideration the previous timer
+  // may want to change this
+  const restart = () => {
+    tick();
+  };
+
+  const pause = () => {
+    cleanUp();
   };
 
   const cleanUp = () => clearInterval(interval);
 
   const setUp = () => {
-    interval = setInterval(refresh, timeout);
+    tick();
     return cleanUp;
   };
 
-  const store = writable<PromiseLike<T>>(Promise.resolve(initialData), setUp);
+  const store = writable<RefreshableStore<T>>(
+    {
+      data: Promise.resolve(initialData),
+      get timeRemaining() {
+        return this.endTime - Date.now();
+      },
+      get percentCompleted() {
+        return Math.floor(((timeout - this.timeRemaining) / timeout) * 100);
+      },
+      startTime: Date.now(),
+      endTime: Date.now() + timeout,
+      currentState: 'other',
+    },
+    setUp,
+  );
 
   return {
     subscribe: store.subscribe,
     refresh,
+    restart,
+    pause,
   };
 };
--- src/routes/namespaces/[namespace]/workflows/index.svelte
+++ src/routes/namespaces/[namespace]/workflows/index.svelte
@@ -50,7 +50,25 @@
 
 <h2 class="text-2xl">Workflows</h2>
 <WorkflowFilters bind:timeFormat />
-{#await $data}
+{$data.timeRemaining}
+{$data.percentCompleted} %
+<div class="grid gap-5 grid-cols-12">
+  <button
+    on:click={data.pause}
+    class="rounded bg-blue-500 text-offWhite p-2 col-span-6">Pause</button
+  >
+  <button
+    on:click={data.restart}
+    class="rounded bg-blue-500 text-offWhite p-2 col-span-6">Restart</button
+  >
+</div>
+<div class="w-full">
+  <div
+    class="h-10 bg-blueGray-500 transition-width ease-in duration-200	"
+    style:width={$data.percentCompleted + '%'}
+  />
+</div>
+{#await $data.data}
   <WorkflowsLoadingState />
 {:then { workflows }}
   {#if workflows.length}
